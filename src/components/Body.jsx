734c2cf5cc5b1a816c53d080ac56483db95d6def
GSMFURQAN <52992635+GSMFURQAN@users.noreply.github.com>
2022-05-15 18:45:39 +0530

    mobile responsive

--- src/components/Body.jsx
+++ src/components/Body.jsx
@@ -1,39 +1,47 @@
 import React from "react";
 import { useState } from "react";
 import '../App.css'
+import temp from "../images/temp.png"
+import prec from "../images/prec.png"
+import wind from "../images/wind.png"
 
 export default function Body() {
     const [option, setOption] = useState("");
-    const [imge, setImge] = useState("temp.png")
+    const [imge, setImge] = useState(temp)
   const  handleClick = (a) =>{
         if(a === "temp"){
-            setImge("temp.png")
+            setImge(temp)
         }else if(a === "prec"){
-            setImge("prec.png")
+            setImge(prec)
         }else{
-            setImge("wind.png")
+            setImge(wind)
         }
     }
   return (
     <>
-      <div className="container" style={{display:"flex"}} >
-          <h3>
-          <button className="text-white btn btn-dark fs-3" onClick={()=> handleClick("temp")}>Temperature &nbsp;<span className="fs-3 fw-lighter text-secondary"> |</span></button>
-          </h3>
+      <div className="container buttons"  >
+          <button type="button" className="text-white btn btn-dark fs-3" onClick={()=> handleClick("temp")}>  <h3> Temperature  
           
-          <h3>
-          <button className="text-white btn btn-dark fs-3" onClick={()=> handleClick("prec")}>Precipitation &nbsp;<span className="fs-3 fw-lighter text-secondary"> |</span></button>
-          </h3>
-        
-          <h3>
-          <button className="text-white btn btn-dark fs-3" onClick={()=> handleClick("wind")}>Wind<span className="fs-3 fw-lighter text-secondary"> |</span></button>
-          </h3>
-        
-        <a href={`https://www.google.com/search?q=weather&rlz=1C1ONGR_enIN972IN972&oq=we&aqs=chrome.0.69i59j69i57j35i39j46i199i433i465i512j69i60l4.1818j1j9&sourceid=chrome&ie=UTF-8`}><button className="text-white btn btn-dark fs-3" onClick={()=> handleClick("wind")}>Go Online</button></a>
-        
+          &nbsp;<span className="fs-3 fw-lighter text-secondary"> |</span></h3>
+          </button>
+         
           
           
+          <button type="button" className="text-white btn btn-dark fs-3" onClick={()=> handleClick("prec")}> <h3>Precipitation
+           &nbsp;<span className="fs-3 fw-lighter text-secondary"> |</span></h3>
+           </button>
+          
+        
+         
+          <button type="button" className="text-white btn btn-dark fs-3" onClick={()=> handleClick("wind")}>  <h3>Wind   &nbsp;
+          <span className="fs-3 fw-lighter text-secondary"> |</span></h3>
+          </button>
+        
+        
+        <a href={`https://www.google.com/search?q=weather&rlz=1C1ONGR_enIN972IN972&oq=we&aqs=chrome.0.69i59j69i57j35i39j46i199i433i465i512j69i60l4.1818j1j9&sourceid=chrome&ie=UTF-8`}><button type="button" className="text-white btn btn-dark fs-3" onClick={()=> handleClick("wind")}> <h3>Go Online</h3> </button></a>
       </div>
+
+
 
       <div className="container"> 
           <img src={imge} alt="" style={{width:"100%", height:"auto"}} />
--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,36 +1,41 @@
 import React from "react";
 import Body from "./Body";
+import cloudsun from "../images/cloudsun.png";
 
 export default function Home() {
   return (
-    <div >
-
-      <div className=" container py-1 bg-dark text-white">
-        <div className="row g-0">
-          <div className="card-body" style={{ display: "flex" }}>
-            <img
-              src="cloudsun.png"
-              className="img-fluid rounded-start py-5 px-2"
-              alt="..."
-            />
-            <div>
-              <h5
-                className="card-title mx-2 my-4 py-4 px-0"
-                style={{ fontSize: "5rem" }}
+    <div>
+      <div className="card container mb-3 pt-5 pb-3 bg-dark ">
+        <div className="row g-0 bg-dark">
+          <div className="col-lg-3">
+            <span style={{ display: "flex" }}>
+              <img
+                src={cloudsun}
+                className="img-fluid rounded-start"
+                alt="..."
+                style={{ height: "80%" }}
+              />
+              <h1
+                className="ms-1 py-2 "
+                style={{ fontSize: "4rem", width: "35%" }}
               >
                 43<sup className="fw-light">°</sup>
-                <sup className="fw-light">c</sup>
-              </h5>
-            </div>
-            <div className="py-5 m-3 text-secondary">
-              <h5 className="card-title">Precipitation: 0%</h5>
-              <h5 className="card-title">Humidity: 16%</h5>
-              <h5 className="card-title">Wind: 8 km/h</h5>
+                <sup className="fw-lighter">c</sup>
+              </h1>
+            </span>
+          </div>
+
+          <div className="col-lg-8">
+            <div className="card-body">
+              <div className="text-secondary">
+                <h5 className="card-title">Precipitation: 0%</h5>
+                <h5 className="card-title">Humidity: 16%</h5>
+                <h5 className="card-title">Wind: 8 km/h</h5>
+              </div>
             </div>
           </div>
         </div>
       </div>
-
 
       <div>
         <Body />
